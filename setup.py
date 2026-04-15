fec23928adba3ffbd80f1a7e53429370b94f9cd7
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-04-15 17:32:12 -0400

    package orientation for crmflx

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+#: pyproject.toml tool.setuptools.packages.find config specifying "src"
+#: as the source to build packages, but this config is separate from
+#: cythonize build path config. Must specify twice.
+
+extensions = [
+    Extension("crmflx.crmflx", ["src/crmflx/crmflx.pyx"])
+]
+
+setup(
+    ext_modules = cythonize(extensions, force=True)
+)
